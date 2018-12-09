96baa97381ace9fe425a5d473ad44d08ff18f984
Lingraj Mahanand <mlingraj@medipedia.io>
2018-12-09 22:44:43 +0530

    Send Grid mail implementation

--- src/main.go
+++ src/main.go
@@ -6,9 +6,12 @@ import (
 
 	ctrl "./controller"
 	"github.com/gorilla/mux"
+
+	u "./utils"
 )
 
 func main() {
+	u.SendEmail()
 	router := mux.NewRouter()
 	router.HandleFunc("/email/list", ctrl.GetEmails).Methods("GET")
 	router.HandleFunc("/email", ctrl.SendEmail).Methods("POST")
