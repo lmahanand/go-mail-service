836d134a4636e85b351485ecede729cdf1aa1055
Lingraj Mahanand <mlingraj@medipedia.io>
2018-12-11 14:02:32 +0530

    test case 2

--- src/utils/utils_test.go
+++ src/utils/utils_test.go
@@ -6,7 +6,11 @@ import (
 
 func TestMessage(t *testing.T) {
 
-	if Message(true, "Test Data")["status"] != true && Message(true, "Test Data")["message"] == "Test Data" {
+	if Message(true, "Test Data")["status"] != true {
 		t.Error("Expected status equal true")
+	}
+
+	if Message(true, "Test Data")["message"] != "Test Data" {
+		t.Error("Expected message equal Test Data")
 	}
 }
